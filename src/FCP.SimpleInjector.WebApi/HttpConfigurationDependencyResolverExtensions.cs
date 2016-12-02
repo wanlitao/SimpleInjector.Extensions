67981492c6de1b63ecb462c61cf7ca3067fb1013
万里涛 <wanlitao_3055601@outlook.com>
2016-12-02 20:20:56 +0800

    add FCP WebApi Integration

---
+++ src/FCP.SimpleInjector.WebApi/HttpConfigurationDependencyResolverExtensions.cs
@@ -0,0 +1,31 @@
+﻿using SimpleInjector;
+using SimpleInjector.Extensions.AssemblyScan;
+using SimpleInjector.Integration.WebApi;
+using System;
+using System.Web.Http;
+
+namespace FCP.SimpleInjector.WebApi
+{
+    public static class HttpConfigurationDependencyResolverExtensions
+    {
+        public static HttpConfiguration UseSimpleInjector(this HttpConfiguration configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
+
+            container.RegisterCompositionRoots("*Composition.dll");
+            container.ResolveUnregisteredTypeByScanDefineAssembly(Lifestyle.Scoped);
+
+            container.RegisterWebApiControllers(configuration);
+
+            container.Verify();
+
+            configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
+            return configuration;
+        }
+    }
+}
