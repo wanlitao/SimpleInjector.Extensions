e5876ecd3f4833f8137cde0effd30205ea2969fb
万里涛 <wanlitao_3055601@outlook.com>
2016-12-02 20:38:29 +0800

    add composition search pattern parameter

--- src/FCP.SimpleInjector.WebApi/HttpConfigurationDependencyResolverExtensions.cs
+++ src/FCP.SimpleInjector.WebApi/HttpConfigurationDependencyResolverExtensions.cs
@@ -8,15 +8,21 @@ namespace FCP.SimpleInjector.WebApi
 {
     public static class HttpConfigurationDependencyResolverExtensions
     {
-        public static HttpConfiguration UseSimpleInjector(this HttpConfiguration configuration)
+        /// <summary>
+        /// Use SimpleInjector DependencyResolver
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="compositionSearchPattern">composition dll search pattern</param>
+        /// <returns></returns>
+        public static HttpConfiguration UseSimpleInjector(this HttpConfiguration configuration, string compositionSearchPattern)
         {
             if (configuration == null)
-                throw new ArgumentNullException(nameof(configuration));
+                throw new ArgumentNullException(nameof(configuration));            
 
             var container = new Container();
             container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
 
-            container.RegisterCompositionRoots("*Composition.dll");
+            container.RegisterCompositionRoots(compositionSearchPattern);
             container.ResolveUnregisteredTypeByScanDefineAssembly(Lifestyle.Scoped);
 
             container.RegisterWebApiControllers(configuration);
