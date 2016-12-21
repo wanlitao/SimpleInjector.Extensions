3cc9c781a8f52e7ab52d4ea7fca6dbe7aec4e292
万里涛 <wanlitao_3055601@outlook.com>
2016-12-21 20:45:09 +0800

    remove un used reference

--- src/FCP.SimpleInjector.WebApi/Extensions/HttpConfigurationDependencyResolverExtensions.cs
+++ src/FCP.SimpleInjector.WebApi/Extensions/HttpConfigurationDependencyResolverExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using SimpleInjector;
 using SimpleInjector.Extensions.AssemblyScan;
-using SimpleInjector.Extensions.LifetimeScoping;
 using SimpleInjector.Integration.WebApi;
 using System;
 using System.Web.Http;
@@ -20,16 +19,11 @@ namespace FCP.SimpleInjector.WebApi
             if (configuration == null)
                 throw new ArgumentNullException(nameof(configuration));            
 
-            var container = new Container();
-
-            //support dependency resolver outside Web Api Request Context
-            var webApiLifestyle = new WebApiRequestLifestyle();
-            container.Options.DefaultScopedLifestyle = Lifestyle.CreateHybrid(
-                () => webApiLifestyle.GetCurrentScope(container) != null,
-                webApiLifestyle, new LifetimeScopeLifestyle());
+            var container = new Container();            
+            container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
 
             container.ResolveUnregisteredTypeByScanDefineAssembly(Lifestyle.Scoped);
-            container.RegisterCompositionRoots(compositionSearchPattern);            
+            container.RegisterCompositionRoots(compositionSearchPattern);
 
             container.RegisterWebApiControllers(configuration);
 
